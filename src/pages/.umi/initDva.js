b7be897cc47e7ff5513da9a985bd2ecf3681a2e8
linxinqiang <linxinqiang@git.xrjiot.cn>
2018-12-05 14:28:20 +0800

    first commit

---
+++ src/pages/.umi/initDva.js
@@ -0,0 +1,28 @@
+import dva from 'dva';
+import createLoading from 'dva-loading';
+
+const runtimeDva = window.g_plugins.mergeConfig('dva');
+let app = dva({
+  history: window.g_history,
+  
+  ...(runtimeDva.config || {}),
+});
+
+window.g_app = app;
+app.use(createLoading());
+(runtimeDva.plugins || []).forEach(plugin => {
+  app.use(plugin);
+});
+
+app.model({ namespace: 'global', ...(require('/home/git/blank-frontend/src/models/global.js').default) });
+app.model({ namespace: 'list', ...(require('/home/git/blank-frontend/src/models/list.js').default) });
+app.model({ namespace: 'login', ...(require('/home/git/blank-frontend/src/models/login.js').default) });
+app.model({ namespace: 'menu', ...(require('/home/git/blank-frontend/src/models/menu.js').default) });
+app.model({ namespace: 'project', ...(require('/home/git/blank-frontend/src/models/project.js').default) });
+app.model({ namespace: 'setting', ...(require('/home/git/blank-frontend/src/models/setting.js').default) });
+app.model({ namespace: 'user', ...(require('/home/git/blank-frontend/src/models/user.js').default) });
+app.model({ namespace: 'register', ...(require('/home/git/blank-frontend/src/pages/User/models/register.js').default) });
+app.model({ namespace: 'activities', ...(require('/home/git/blank-frontend/src/pages/Dashboard/models/activities.js').default) });
+app.model({ namespace: 'chart', ...(require('/home/git/blank-frontend/src/pages/Dashboard/models/chart.js').default) });
+app.model({ namespace: 'monitor', ...(require('/home/git/blank-frontend/src/pages/Dashboard/models/monitor.js').default) });
+app.model({ namespace: 'geographic', ...(require('/home/git/blank-frontend/src/pages/Account/Settings/models/geographic.js').default) });
---
+++ src/pages/.umi/router.js
@@ -0,0 +1,144 @@
+import React from 'react';
+import { Router as DefaultRouter, Route, Switch } from 'react-router-dom';
+import dynamic from 'umi/dynamic';
+import renderRoutes from 'umi/_renderRoutes';
+import RendererWrapper0 from '/home/git/blank-frontend/src/pages/.umi/LocaleWrapper.jsx'
+
+let Router = require('dva/router').routerRedux.ConnectedRouter;
+
+let routes = [
+  {
+    "path": "/user",
+    "component": dynamic({ loader: () => import('../../layouts/UserLayout'), loading: require('/home/git/blank-frontend/src/components/PageLoading/index').default }),
+    "routes": [
+      {
+        "path": "/user",
+        "redirect": "/user/login",
+        "exact": true
+      },
+      {
+        "path": "/user/login",
+        "component": dynamic({ loader: () => import('../User/Login'), loading: require('/home/git/blank-frontend/src/components/PageLoading/index').default }),
+        "exact": true
+      },
+      {
+        "path": "/user/register",
+        "component": dynamic({ loader: () => import('../User/Register'), loading: require('/home/git/blank-frontend/src/components/PageLoading/index').default }),
+        "exact": true
+      },
+      {
+        "path": "/user/register-result",
+        "component": dynamic({ loader: () => import('../User/RegisterResult'), loading: require('/home/git/blank-frontend/src/components/PageLoading/index').default }),
+        "exact": true
+      },
+      {
+        "component": () => React.createElement(require('/home/git/blank-frontend/node_modules/_umi-build-dev@1.2.6@umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
+      }
+    ]
+  },
+  {
+    "path": "/",
+    "component": dynamic({ loader: () => import('../../layouts/BasicLayout'), loading: require('/home/git/blank-frontend/src/components/PageLoading/index').default }),
+    "Routes": [require('../Authorized').default],
+    "authority": [
+      "admin",
+      "user"
+    ],
+    "routes": [
+      {
+        "path": "/",
+        "redirect": "/dashboard/workplace",
+        "exact": true
+      },
+      {
+        "path": "/dashboard",
+        "name": "dashboard",
+        "icon": "dashboard",
+        "hide": "true",
+        "routes": [
+          {
+            "path": "/dashboard/workplace",
+            "name": "workplace",
+            "component": dynamic({ loader: () => import('../Dashboard/Workplace'), loading: require('/home/git/blank-frontend/src/components/PageLoading/index').default }),
+            "exact": true
+          },
+          {
+            "component": () => React.createElement(require('/home/git/blank-frontend/node_modules/_umi-build-dev@1.2.6@umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
+          }
+        ]
+      },
+      {
+        "name": "account",
+        "icon": "user",
+        "path": "/account",
+        "routes": [
+          {
+            "path": "/account/center",
+            "name": "center",
+            "component": dynamic({ loader: () => import('../Account/Center/Center'), loading: require('/home/git/blank-frontend/src/components/PageLoading/index').default }),
+            "exact": true
+          },
+          {
+            "path": "/account/settings",
+            "name": "settings",
+            "component": dynamic({ loader: () => import('../Account/Settings/Info'), loading: require('/home/git/blank-frontend/src/components/PageLoading/index').default }),
+            "routes": [
+              {
+                "path": "/account/settings",
+                "redirect": "/account/settings/base",
+                "exact": true
+              },
+              {
+                "path": "/account/settings/base",
+                "component": dynamic({ loader: () => import('../Account/Settings/BaseView'), loading: require('/home/git/blank-frontend/src/components/PageLoading/index').default }),
+                "exact": true
+              },
+              {
+                "path": "/account/settings/security",
+                "component": dynamic({ loader: () => import('../Account/Settings/SecurityView'), loading: require('/home/git/blank-frontend/src/components/PageLoading/index').default }),
+                "exact": true
+              },
+              {
+                "path": "/account/settings/binding",
+                "component": dynamic({ loader: () => import('../Account/Settings/BindingView'), loading: require('/home/git/blank-frontend/src/components/PageLoading/index').default }),
+                "exact": true
+              },
+              {
+                "path": "/account/settings/notification",
+                "component": dynamic({ loader: () => import('../Account/Settings/NotificationView'), loading: require('/home/git/blank-frontend/src/components/PageLoading/index').default }),
+                "exact": true
+              },
+              {
+                "component": () => React.createElement(require('/home/git/blank-frontend/node_modules/_umi-build-dev@1.2.6@umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
+              }
+            ]
+          },
+          {
+            "component": () => React.createElement(require('/home/git/blank-frontend/node_modules/_umi-build-dev@1.2.6@umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
+          }
+        ]
+      },
+      {
+        "component": dynamic({ loader: () => import('../404'), loading: require('/home/git/blank-frontend/src/components/PageLoading/index').default }),
+        "exact": true
+      },
+      {
+        "component": () => React.createElement(require('/home/git/blank-frontend/node_modules/_umi-build-dev@1.2.6@umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
+      }
+    ]
+  },
+  {
+    "component": () => React.createElement(require('/home/git/blank-frontend/node_modules/_umi-build-dev@1.2.6@umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
+  }
+];
+window.g_plugins.applyForEach('patchRoutes', { initialValue: routes });
+
+export default function() {
+  return (
+<RendererWrapper0>
+          <Router history={window.g_history}>
+      { renderRoutes(routes, {}) }
+    </Router>
+        </RendererWrapper0>
+  );
+}
